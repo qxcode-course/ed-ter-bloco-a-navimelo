38cad1e2e0ce1dc4bc31a804d37ad8ba927ffe8c
navimelo <vitoriamelo9373@gmail.com>
2026-03-22 01:36:18 +0000

    salvar210326

--- ed/gomos/go/draft.go
+++ ed/gomos/go/draft.go
@@ -1,5 +1,38 @@
 package main
 import "fmt"
-func main() {
-    fmt.Println("qxcode")
+
+type Gomo struct {
+    x, y int
+}
+
+func main() { 
+    qtd := 0
+    dir := ""
+
+    fmt.Scan(&qtd, &dir)
+
+    cobra := make([]Gomo, qtd)
+
+    for i := range cobra {
+        fmt.Scan(&cobra[i].x, &cobra[i].y)
+    }
+
+    for i:= qtd - 1; i > 0; i--{
+        cobra[i] = cobra[i-1]
+    }
+
+    switch dir {
+    case "L":
+        cobra[0].x--
+    case "R":
+        cobra[0].x++
+    case "U":
+        cobra[0].y--
+    case "D":
+        cobra[0].y++
+    }
+
+    for _, gomo := range cobra{
+        fmt.Printf("%v %v\n", gomo.x, gomo.y)
+    }
 }
