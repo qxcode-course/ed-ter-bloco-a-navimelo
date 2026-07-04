3a1f55ea826ecb53d94bc3f613c4b6fdedbb679d
navimelo <vitoriamelo9373@gmail.com>
2026-07-04 05:41:07 +0000

    salvar0407

--- ed/wordsearch/src/go/main.go
+++ ed/wordsearch/src/go/main.go
@@ -6,9 +6,45 @@ import (
 	"os"
 )
 
+func busca(grid [][]byte, l, c int, palavra string, idx int) bool{
+	if idx == len(palavra){
+		return true
+	}
+	if l < 0 || l >= len(grid) || c < 0 || c >= len(grid[0]) || grid[l][c] != palavra[idx]{
+		return false
+	}
+	let := grid[l][c]
+	grid[l][c] = ' '
+
+	dirl := []int{-1, 1, 0, 0}
+	dirc := []int{ 0, 0, 1, -1}
+
+	for i := 0; i <4; i++{
+		proxl := l + dirl[i]
+		proxc := c + dirc[i]
+
+		if busca(grid, proxl, proxc, palavra, idx + 1){
+			return true
+		}
+	}
+	grid[l][c] = let
+	return false
+}
+
 // Não mude a assinatura desta função, ela é a função chamada pelo LeetCode
 func exist(grid [][]byte, word string) bool {
-	_, _ = grid, word
+	if len(grid) == 0 || len(word) == 0{
+		return false
+	}
+	for l := 0; l < len(grid); l++{
+		for c := 0; c < len(grid[0]); c++{
+			if grid[l][c] == word[0]{
+				if busca(grid, l, c, word, 0){
+					return true
+				}
+			}
+		}
+	}
 	return false
 }
 
